ad53c59b53f068d0a64958d57926560422ac2fa1
AndreiR41 <148470836+AndreiR41@users.noreply.github.com>
2025-04-30 19:46:04 -0300

    Fazer recomeçar a pilha
    Modifiquei muita coisa baseada na exibição do contador, só que acabou ficando muito parecido com o de JV ( não tem tanto pra onde fugir)

--- Lab1/Lab1/main.asm
+++ Lab1/Lab1/main.asm
@@ -1,42 +1,78 @@
+
 ;
-; Lab1.asm
-;
-; Created: 31/03/2025 16:45:13
-; Author : andre
-;
+	.include "m328pdef.inc"
+
+	.equ	numB 	= 10		; number of bytes in array
+	.dseg
+	.org	SRAM_START
+
+	;=================================================PILHA==================================================
+sArr:	.BYTE	numB			; allocate bytes in SRAM for array
+
+	.cseg
+	.org	0x00
+
+;=================================================SAÍDAS==================================================
+organiza:
+	; CONFIGURANDO PINOS DE SAÍDA
+	LDI R23,(1<<PB0)
+	OUT DDRB,R23     ; PB0 COMO SAÍDA
+	LDI R23,(1<<PD2)|(1<<PD3)|(1<<PD4)|(1<<PD5)|(1<<PD6)|(1<<PD7)
+	OUT DDRD,R23  ; PD7:PD2
 
 
 
+; carregar dados da memoria de programa só pode ser feito de forma indireta com ponteiros!!!
+;// vezes dois para fazer deslocamento a esquerda e consertar endereço
+
+	;//usado para acessar o array na memoria de programa entao precisa converter de palavra p byte
+	ldi	YL,LOW(2*pArr)		; inicia o ponteiro do array
+	ldi	YH,HIGH(2*pArr)		; converte de palavra para byte
+loop:
+; carrea em XL para usar o mesmo binbcd do contador (adaptação do lab1 antigo)
+	lpm XL,Y+           ; carrega e incrementa Y
+	RCALL subcent
+	RCALL exibir
+	;comparar se chegou no ultimo valor (numbB==10) e se sim voltar pra o começo da pilha
+	rjmp loop
 
 
-.ORG 0x000    // enderco de inicio de escrita
-
-;FALTA FAZER A FILA
-; tudo funcionando aqui pra baixo
-main:
 	
-	LDI R16,0xFF // carrega o valor 255
-	;seta tudo em saida
-	OUT DDRD,R16 //configura portB como saida
-	LDI R17,R23 //numero que vai colocar
-	RJMP subcent
 
-exibir:
-	MOV R17, R13
-	;escreve o valor de r17 na port D
-	OUT PORTD,R17
-	RJMP atraso
 
-	MOV R17, R14
-	RJMP atraso
+; Espaço alocado tem que ser um número par!
+;//array da memoria de programa
+pArr:	.db	173,1,255,3,4,5,36,7,84,9		; program memory array //db é para carregar bits
 
-	MOV R17, R15
-	RCALL atraso   //chama atraso
-	RJMP main // pula de volta pra principal
 
-	
+
+
+
+
+
+;=================================================BIN-BCD==================================================
+
+	;FAZER FUNCAO EXIBIR NOS DISPLAYS
+	; ABSURDAMENTE ERRADO ESTAMOS PERDENDO A INFORMAÇÃO NO X (ATUAL PARA EXIBIR, ENTÃO FICAMOS CONTANDO DO 0 ATÉ O 1)
+	;ARMAZENAR TEMPORIAMENTE O ATUAL PARA REALIZAR OPERAÇÕES E COMPARAÇÕES !!!!
+
+	; 3 reg cent,dez,uni de 8 bits. Soma eles com respectivo valor 
+	;Os 4 LSBs (r16[3:0]) sejam copiados para os pinos PD7, PD6, PD5, PD4 (em ordem).
+	;Os 3 MSBs (r16[7:5]) sejam copiados para os pinos PB0, PD3, PD2 (em ordem).
 
 subcent:
+	; R16 <- XH , R17 <- XL
+	MOV R16,XH
+	MOV R17,XL
+	CPI XH,0x00
+	BREQ subcentlow
+	SUBI R16,50
+	SBCI R17,50
+	INC R13
+
+subcentlow:
+	CPI  R17,0x64
+	BRLO subdez
 	SUBI R17,0x64
 	INC R13
 	CPI  R17,0x64
@@ -44,6 +80,8 @@ subcent:
 	BRLO subdez
 
 subdez:
+	CPI R17,0x0A
+	BRLO subuni
 	SUBI R17,0x0A
 	INC R14
 	CPI R17,0x0A
@@ -51,49 +89,105 @@ subdez:
 	BRLO subuni
 
 subuni:
-;comparando se der ruim
 	CPI R17,0x00
-	BREQ organiza
+	BREQ retorno
 
 	SUBI R17,0x01
 	INC R15
 	CPI R17,0x01
 	BRGE subuni
-	BRLO organiza
+	RET	
+retorno:                   ; Tive que seguir os passos da lenda...
+	ret
 
-organiza:
-	SWAP R13
-	;ADIW R00,100
-	SWAP R14
-	;ADIW R01,010
-	SWAP R15
-	;ADIW R02,001
-	RJMP exibir
+;=================================================EXIBIÇÃO SAÍDAS==================================================
+exibir:    ;R17 temporario    ===== R13 Centena, R14 Dezena , R13 Unidade
 
+CENTENA:
+	MOV R17,R13
+	RCALL mirror
+	SBI PORTB,0
+	CBI PORTD,3
+	CBI PORTD,2
+	;RCALL delaymin
+DEZENA:
+	MOV R17,R14
+	RCALL mirror
+	CBI PORTB,0
+	SBI PORTD,3
+	CBI PORTD,2
+	;RCALL delaymin
+UNIDADE:
+	MOV R17,R15
+	RCALL mirror
+	CBI PORTB,0
+	CBI PORTD,3
+	SBI PORTD,2
+	;RCALL delaymin
 
+	;vai contando 50 vezes para piscar
+	INC R18
+	CPI R18, 0x32
+	BRLO exibir 
 
-;255x255x255
-atraso:
+	RET
+
+;=================================================SWAP e Espelhamento==================================================
+mirror:
+	
+	SWAP R17; Trocamos as posições para poder mandar pros displays
+	; troca os valores da posição dos bits
+	; primeiro setamos tudo em zero
+	LDI R19, 0x00 
+	OUT PORTD,R19 ; Setamos todas as saídas com zero
+	;fazemos a troca de posições e a saídas diretamente
+	SBRC R17,7;esse comando pula a proxima linha se o bit indicado no registrador estiver limp
+	SBI  PORTD, 4;nesse comando setamos o bit especifico na porta para exibição com base na checagem de R17
+	; para portd5
+	SBRC R17,6
+	SBI  PORTD, 5
+	; para portd6
+	SBRC R17,5
+	SBI  PORTD, 6
+	; para portd7
+	SBRC R17,4
+	SBI  PORTD, 7
+	RET
+
+	
+
+;=================================================DELAY==================================================	
+delay:
 	LDI R18,0x00
 	LDI R19,0X00
 	LDI R20,0x00
 
-volta:
+delayloop:
 	DEC R18
-	BRNE volta //enquanto R17>0 fica decrementando
+	BRNE delayloop //enquanto R17>0 fica decrementando
 
 	DEC R19
-	BRNE volta
+	BRNE delayloop
 
 	DEC R20
-	BRNE volta
+	BRNE delayloop
 
-	RET// retorna da sub-rotina
+	RET
 
+delaymin:
+	
+	LDI R18,0x00
+	LDI R19,0X00
 
+delayminloop:
+	DEC R18
+	BRNE delayminloop
 
+	DEC R19
+	BRNE delayminloop
 
-
-
-
-
+	RET
+	
+; 255*255*255 = 16581375 ~ 16 mega
+; 255*255 = 65025
+; decrementar 16 milhoes de vezes numa frequencia de 16 mega = 1 segundo
