4f0a3baf253ecb5994284212850761e10143b776
AndreiR41 <148470836+AndreiR41@users.noreply.github.com>
2025-05-10 14:04:02 -0300

    Add files via upload

--- Lab2/Contador_Inteligente/Contador_Inteligente/main.asm
+++ Lab2/Contador_Inteligente/Contador_Inteligente/main.asm
@@ -47,16 +47,16 @@ botoes:
     OUT DDRC, R16
 	LDI R16, 0b00001110     ; Ativa pull-up interno nos pinos PC1, PC2, PC3
 	OUT PORTC, r16
-/*
+; setando os leds como saída
 leds:
 	sbi DDRB, PB1
 	sbi DDRB, PB2
-	sbi DDRB, PB3*/
+	sbi DDRB, PB3
 	
 
 ;=================================================HABILITA INTERRUPÇÃO==================================================
 ; Habilita interrupções PCINT9 (PC1) e PCINT11 (PC3)                          SIMULADOR NÃO FAZ INTERRUPÇÃO
-    LDI r16, (1<<PCINT9) | (1<<PCINT11)
+    LDI r16, (1<<PCINT9) | (1<<PCINT10) | (1<<PCINT11)
     STS PCMSK1, r16      ; Habilita bits individuais
     LDI r16, (1<<PCIE1)
     STS PCICR, r16        ; Habilita grupo PCINT1 (PORTC)
@@ -87,33 +87,46 @@ inicial:
 	; carregando registrador com zero para somar 16 bits com 8 GAMBIARRA
 	LDI R24, 0x00 
 
+	CLR R21 ; 
+
 	 
 ;=================================================CONTAGEM PADRÃO==================================================
 
 
 contagem:
-; FAZER Comparações S1,S2,S3											 por causa do PULL-UP interno se On =0 se Off =1
-	IN R17, PINC            ; Lê estado dos pinos de entrada (PORTC)
+	
+	; FAZER Comparações S1,S2,S3											 por causa do PULL-UP interno se On =0 se Off =1
+	CPI R21,1
+	BREQ incrementar
+	CPI R21,2
+	BREQ decrementar
+	CPI R21,3
+	BREQ minimo
 	; Se PC2 == 1, pula a próxima instrução
 	; Se PC2 == 0 ? botão pressionado
-; Verifica se botão em PC2 está pressionado
-	SBRC R17,PC2
-	RCALL minimo
+	; Verifica se botão em PC2 está pressionado
 	
+	CLR R13
+	CLR R14
+	CLR R15
+	CLR R16
+	CLR R17
+	CLR R18
+	CBI PORTB,PB1
+	CBI PORTB,PB2 
+	CBI PORTB,PB3    ; DESLIGANDO O LED
 
-
-;CONTAGEM PADRÃO
-	LDI R17,0x00 ; LIMPA O TEMP PARA USAR NO SWAP
-	MOV R13,R24
-	MOV R14,R24 ; LIMPANDO OS CONTADORES BCD ANTES DE REALIZAR A PROXIMA EXIBIÇÃO
-	MOV R15,R24 
+	;CONTAGEM PADRÃO
+	;LDI R17,0x00 ; LIMPA O TEMP PARA USAR NO SWAP
 
 	ADD XL, R25
 	ADC XH, R24
+	MOV R16,XH
+	MOV R17,XL
 	RCALL subcent    ; faz o BIN-BCD
+	CLR R22
 	RCALL exibir     ; mostra nos displays
 
-	RCALL delay
 
 	; compara com o maximo
 	CP XL,YL
@@ -121,107 +134,178 @@ contagem:
 	BRLO contagem; se menor volta para contagem
 	BRSH inicial ; se maior ou igual voltar para o inicio
 
+
 ;=================================================INCREMENTAR==================================================
 ; carrega os valores de minimo no atual
 
 incrementar:	
+	CLR R13   ; limpa a memoria do bin bcd
+	CLR R14
+	CLR R15
+	CLR R21
+	LDI R25, 5
+	;
 	ADD XL, R25
 	ADC XH, R24
 
 	CP XL,YL
-	CPC XH,YH
+	CPC XH,YH 
+	BRSH contagem   ; Se maior carrega retorna pra contagem
+	MOV R16,XH
+	MOV R17,XL
 	RCALL subcent
     RCALL exibir
-    RCALL delay
+	
+	RJMP incrementar
 
-	BRLO incrementar; se menor volta para inc
-	RET ; se maior ou igual voltar 
 
 ;=================================================DECREMENTAR==================================================
 decrementar:
+	CLR R13   ; limpa a memoria do bin bcd
+	CLR R14
+	CLR R15
+	CLR R21
 	; subtrai atual com o passo
+	LDI ZL,0x02
+	CLR ZH ; valor minimo 2
+	LDI R25,2
+	;
 	SUB XL, R25
 	SBC XH, R24
 
 	CP XL,ZL
 	CPC XH,ZH
+	BRLO inicial ;se menor  volta pra contagem padrão
+	MOV R16,XH
+	MOV R17,XL
 	RCALL subcent
 	RCALL exibir
-	RCALL delay
-
-	BRSH decrementar ; se maior ou igual voltar para o DEC
-	RET ; se menor voltar 
+	
+	
+	RJMP decrementar ; se maior ou igual voltar para o DEC
+	
 
 ;=================================================S2==================================================
 
 ;INSERIR MINIMO, INSERIR MAXIMO, INSERIR PASSO 
 	
 	minimo:
+		CBI PORTB,PB1    ; LIGANDO O LED
+		CBI PORTB,PB2 
+		SBI PORTB,PB3
 ;		CHAMAR ADC E CARREGAR EM Z O VALOR LIDO
 		RCALL ativar_adc
 		
 		MOV ZL,R18 ; O valor menos significativo de ADC
 		MOV ZH,R19 ; O valor menos significativo de ADC
+		ROR ZH
+		ROR ZL ;1
+		CLC
+		ROR ZH ;2
+		ROR ZL
+		CLC
+		ROR ZH ;3
+		ROR ZL
+		CLC
+		ROR ZH ;4
+		ROR ZL
+		CLC
+		ROR ZH ;5
+		ROR ZL
+		CLC
+		ROR ZH ;6
+		ROR ZL
+		CLC
+		;CONFIGURANDO SÓ PARA O BIN BCD
+		MOV R16,ZH
+		MOV R17,ZL
+		RCALL subcent
 		RCALL exibir
-		;chama delay para esperar um segundo antes de ler os pinos C
-		RCALL delay
+
 		
-		IN R17, PINC 
+	
 		; Verifica se botão em PC2 está pressionado
-		SBRC R17,PC2
+		SBIS  PINC,PC2
 		RCALL Maximo
-		RJMP minimo
+		RJMP  minimo
+
 		
 
 	Maximo:
+		CBI PORTB,PB1
+		SBI PORTB,PB2    ; LIGANDO O LED
+		CBI PORTB,PB3 
+		
 		;CHAMAR ADC E CARREGAR EM Y O VALOR LIDO
 		RCALL ativar_adc
 
 		MOV YL,R18 ; O valor menos significativo de ADC
 		MOV YH,R19 ; O valor menos significativo de ADC
+		ROR ZH
+		ROR ZL ;1
+		CLC
+		ROR ZH ;2
+		ROR ZL
+		CLC
+		ROR ZH ;3
+		ROR ZL
+		CLC
+		ROR ZH ;4
+		ROR ZL
+		CLC
+		ROR ZH ;5
+		ROR ZL
+		CLC
+		ROR ZH ;6
+		ROR ZL
+		CLC
+		;CONFIGURANDO SÓ PARA O BIN BCD
+		MOV R16,YH
+		MOV R17,YL
+		RCALL subcent
 		RCALL exibir
-		;chama delay para esperar um segundo antes de ler os pinos C
-		RCALL delay
 
-		IN R17, PINC 
-		SBRC R17,PC2
+
+		SBIS PINC,PC2
 		RCALL passo
 		RJMP Maximo
 		
 
 	passo:
+		SBI PORTB,PB1
+		CBI PORTB,PB2 
+		CBI PORTB,PB3    ; LIGANDO O LED
+
 		;CHAMAR ADC E CARREGAR EM R25 O VALOR LIDO
 		RCALL ativar_adc
-
-		MOV R25,R18 ; O valor menos significativo de ADC
+		;FAZER UM AND  COM 0XF0 5V/16BITS PARA DIMINUIR RESOLUÇÃO E CONTAR ATÉ PROXIMO DE 15
+		ANDI R19, 0XF0
+		MOV R25,R19 ; O valor mais significativo de ADC
+		RCALL subcent
 		RCALL exibir
-		;chama delay para esperar um segundo antes de ler os pinos C
-		RCALL delay
-
-		IN R17, PINC 
-		SBRC R17,PC2
-		RJMP contagem
-		RJMP passo
 		
+		
+		RETI
+		
+		
+		
+
 ;=================================================BIN-BCD==================================================
-
-	;FAZER FUNCAO EXIBIR NOS DISPLAYS
-	; ABSURDAMENTE ERRADO ESTAMOS PERDENDO A INFORMAÇÃO NO X (ATUAL PARA EXIBIR, ENTÃO FICAMOS CONTANDO DO 0 ATÉ O 1)
-	;ARMAZENAR TEMPORIAMENTE O ATUAL PARA REALIZAR OPERAÇÕES E COMPARAÇÕES !!!!
-
-	; 3 reg cent,dez,uni de 8 bits. Soma eles com respectivo valor 
+	;3 reg cent,dez,uni de 8 bits. Soma eles com respectivo valor 
 	;Os 4 LSBs (r16[3:0]) sejam copiados para os pinos PD7, PD6, PD5, PD4 (em ordem).
 	;Os 3 MSBs (r16[7:5]) sejam copiados para os pinos PB0, PD3, PD2 (em ordem).
 
 subcent:
 	; R16 <- XH , R17 <- XL
-	MOV R16,XH
-	MOV R17,XL
 	CPI XH,0x00
-	BREQ subcentlow
+	BREQ subcentlow ;
 	SUBI R16,50
 	SBCI R17,50
 	INC R13
+	;TERMINAR 
+	CPI R16, 0x00
+	BREQ subcentlow
+	RJMP subcent
 
 subcentlow:
 	CPI  R17,0x64
@@ -229,7 +313,7 @@ subcentlow:
 	SUBI R17,0x64
 	INC R13
 	CPI  R17,0x64
-	BRGE subcent
+	BRGE subcentlow
 	BRLO subdez
 
 subdez:
@@ -244,172 +328,124 @@ subdez:
 subuni:
 	CPI R17,0x00
 	BREQ retorno
-
 	SUBI R17,0x01
 	INC R15
 	CPI R17,0x01
 	BRGE subuni
-	RET	
+	
+
 retorno:                   ; Tive que seguir os passos da lenda...
-	ret
+	RET
 
 ;=================================================EXIBIÇÃO SAÍDAS==================================================
-	exibir:    ;R17 temporario    ===== R13 Centena, R14 Dezena , R13 Unidade
-		;CENTENA
-		MOV R17,R13
-		RCALL mirror
-		MOV R13,R17
-		LDI R23,0b000000000
-		ADD R13,R23
-		OUT PORTD, R13
-		SBI PORTB,0
-		RCALL delaymin
-		; DEZENA
-		MOV R17,R14
-		RCALL mirror
-		LDI R23,0b000001000
-		MOV R14,R17
-		ADD R14,R23
-		OUT PORTD, R14
-		CBI PORTB,0
-		RCALL delaymin
-		; UNIDADE
-		MOV R17,R15
-		RCALL mirror
-		LDI R23,0b000000100
-		MOV R15,R17
-		ADD R15,R23
-		OUT PORTD, R15
-		CBI PORTB,0
+exibir:    ;R17 temporario    ===== R13 Centena, R14 Dezena , R15 Unidade
 	
-		;vai contando 50 vezes para piscar
-		INC R25
-		CPI R25,50
-		BRLO exibir 
+CENTENA:
+	MOV R17,R13
+	RCALL mirror
+	SBI PORTB,0
+	CBI PORTD,3
+	CBI PORTD,2
+	RCALL delaymin
+DEZENA:
+	MOV R17,R14
+	RCALL mirror
+	CBI PORTB,0
+	SBI PORTD,3
+	CBI PORTD,2
+	RCALL delaymin
+UNIDADE:
+	MOV R17,R15
+	RCALL mirror
+	CBI PORTB,0
+	CBI PORTD,3
+	SBI PORTD,2
+	RCALL delaymin
 
-	RET
+	;vai contando 50 vezes para piscar
+	INC R22
+	CPI R22, 50
+	BREQ retorno
+	RJMP exibir	
 
-;=================================================SWAP==================================================
+;=================================================SWAP e Espelhamento==================================================
 mirror:
-	;MOV R17, R13  ; EXIBINDO CENTENA 
-	SWAP R17
-	; COMPARA COM ZERO ATÉ NOVE
+	
+	SWAP R17; Trocamos as posições para poder mandar pros displays
+	; troca os valores da posição dos bits
+	CLR R24
+	OUT PORTD,R24 ; Setamos todas as saídas com zero
+	;fazemos a troca de posições e a saídas diretamente
+	SBRC R17,7;esse comando pula a proxima linha se o bit indicado no registrador estiver limp
+	SBI  PORTD, 4;nesse comando setamos o bit especifico na porta para exibição com base na checagem de R17
+	; para portd5
+	SBRC R17,6
+	SBI  PORTD, 5
+	; para portd6
+	SBRC R17,5
+	SBI  PORTD, 6
+	; para portd7
+	SBRC R17,4
+	SBI  PORTD, 7
+	RET
 
-	CPI R17, 0x00
-	BREQ N0
-
-	CPI R17, 0x10
-	BREQ N1
-
-	CPI R17, 0x20
-	BREQ N2
-
-	CPI R17, 0x30
-	BREQ N3
-
-	CPI R17, 0x40
-	BREQ N4
-
-	CPI R17, 0x50
-	BREQ N5
-
-	CPI R17, 0x60
-	BREQ N6
-
-	CPI R17, 0x70
-	BREQ N7
-
-	CPI R17, 0x80
-	BREQ N8
-
-	CPI R17, 0x90
-	BREQ N9
 
 	
-	
 
-;=================================================ESPELHAMENTO==================================================
-N0:
-	LDI R17, 0x00
-	RET
-N1:
-	LDI R17, 0x80
-	RET
-N2:
-	LDI R17, 0x40
-	RET
-N3:
-	LDI R17, 0xC0  ;doze
-	RET
-N4:
-	LDI R17, 0x20
-	RET
-N5:
-	LDI R17, 0xA0
-	RET
-N6:
-	LDI R17, 0x60
-	RET
-N7:
-	LDI R17, 0xE0
-	RET
-N8:
-	LDI R17, 0x10
-	RET
-N9:
-	LDI R17, 0x90
-	RET
-
-	
 
 ;=================================================DELAY==================================================	
 delay:
-	LDI R18,0x00
-	LDI R19,0X00
-	LDI R20,0x00
+	CLR R10
+	CLR R11
+	CLR R12
 
 delayloop:
-	DEC R18
+	DEC R10
 	BRNE delayloop //enquanto R17>0 fica decrementando
 
-	DEC R19
+	DEC R11
 	BRNE delayloop
 
-	DEC R20
+	DEC R12
 	BRNE delayloop
 
 	RET
 
 delaymin:
 	
-	LDI R18,0x00
-	LDI R19,0X00
+	CLR R10
+	CLR R11
 
 delayminloop:
-	DEC R18
+	DEC R10
 	BRNE delayminloop
 
-	DEC R19
+	DEC R11
 	BRNE delayminloop
 
 	RET
 	
 ; 255*255*255 = 16581375 ~ 16 mega
+; 255*255 = 65025
 ; decrementar 16 milhoes de vezes numa frequencia de 16 mega = 1 segundo
 
 
 ;=================================================ROTINA DE INTERRUPÇÃO==================================================
 ; ENTENDER O SENTIMENTO DA COISA
 PCINT_ISR:
-	RCALL delay
-    IN R17, PINC          ; Lê o estado atual do PINC
-
+	CLR R21
     ; Checa se PC1 (PCINT9) está em nível baixo (botão pressionado)
-    SBRC R17, PC1
-    RCALL incrementar
+    SBIS PINC, PC1   ; CHECA AS PORTAS DE IO
+    LDI  R21, 1
+
+	 ; Checa se PC2 (PCINT10) está em nível baixo (botão pressionado)
+    SBIS PINC, PC2
+	LDI R21,3
+   
 
     ; Checa se PC3 (PCINT11) está em nível baixo (botão pressionado)
-    SBRC R17, PC3
-    RCALL decrementar
+    SBIS PINC, PC3
+	LDI  R21, 2
+   
 
     RETI
